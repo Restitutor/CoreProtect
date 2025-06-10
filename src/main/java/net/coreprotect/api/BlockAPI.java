ad542031eb51074cec555beaae26795c538069bb
Restitutor <nissar@mortemale.org>
2025-06-09 19:52:45 -0500

    Added container lookup api

--- src/main/java/net/coreprotect/api/BlockAPI.java
+++ src/main/java/net/coreprotect/api/BlockAPI.java
@@ -1,13 +1,14 @@
 package net.coreprotect.api;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.block.Block;
 
+import net.coreprotect.api.result.ContainerResult;
 import net.coreprotect.config.Config;
 import net.coreprotect.config.ConfigHandler;
 import net.coreprotect.database.Database;
@@ -30,12 +31,41 @@ public class BlockAPI {
     }
 
     /**
+     * Creates a prepared statement with WHERE clause for block/container lookups.
+     *
+     * @param connection   Database connection
+     * @param tableType    Either "block" or "container"
+     * @param selectClause The SELECT portion of the query
+     * @param block        The block to look up
+     * @param checkTime    The minimum time constraint
+     * @return PreparedStatement with parameters set
+     * @throws Exception if there's an error creating the statement
+     */
+    private static PreparedStatement createLocationQuery(Connection connection, String tableType, String selectClause, Block block, int checkTime) throws Exception {
+        int x = block.getX();
+        int y = block.getY();
+        int z = block.getZ();
+        int worldId = WorldUtils.getWorldId(block.getWorld().getName());
+
+        String query = "SELECT " + selectClause + " FROM " + ConfigHandler.prefix + tableType + " " +
+                WorldUtils.getWidIndex(tableType) +
+                "WHERE wid = ? AND x = ? AND z = ? AND y = ? AND time > ? ORDER BY rowid DESC";
+
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, worldId);
+        statement.setInt(2, x);
+        statement.setInt(3, z);
+        statement.setInt(4, y);
+        statement.setInt(5, checkTime);
+
+        return statement;
+    }
+
+    /**
      * Performs a lookup of block-related actions at the specified block.
-     * 
-     * @param block
-     *            The block to look up
-     * @param offset
-     *            Time constraint in seconds (0 means no time constraint)
+     *
+     * @param block  The block to look up
+     * @param offset Time constraint in seconds (0 means no time constraint)
      * @return List of results in a String array format
      */
     public static List<String[]> performLookup(Block block, int offset) {
@@ -65,10 +95,8 @@ public class BlockAPI {
                 checkTime = time - offset;
             }
 
-            try (Statement statement = connection.createStatement()) {
-                String query = "SELECT time,user,action,type,data,blockdata,rolled_back FROM " + ConfigHandler.prefix + "block " + WorldUtils.getWidIndex("block") + "WHERE wid = '" + worldId + "' AND x = '" + x + "' AND z = '" + z + "' AND y = '" + y + "' AND time > '" + checkTime + "' ORDER BY rowid DESC";
-
-                try (ResultSet results = statement.executeQuery(query)) {
+            try (PreparedStatement statement = createLocationQuery(connection, "block", "time,user,action,type,data,blockdata,rolled_back", block, checkTime)) {
+                try (ResultSet results = statement.executeQuery()) {
                     while (results.next()) {
                         String resultTime = results.getString("time");
                         int resultUserId = results.getInt("user");
@@ -85,14 +113,77 @@ public class BlockAPI {
                         String resultUser = ConfigHandler.playerIdCacheReversed.get(resultUserId);
                         String blockData = BlockUtils.byteDataToString(resultBlockData, resultType);
 
-                        String[] lookupData = new String[] { resultTime, resultUser, String.valueOf(x), String.valueOf(y), String.valueOf(z), String.valueOf(resultType), resultData, resultAction, resultRolledBack, String.valueOf(worldId), blockData };
+                        String[] lookupData = new String[]{resultTime, resultUser, String.valueOf(x), String.valueOf(y), String.valueOf(z), String.valueOf(resultType), resultData, resultAction, resultRolledBack, String.valueOf(worldId), blockData};
 
                         result.add(StringUtils.toStringArray(lookupData));
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e) {
+
+        return result;
+    }
+
+    /**
+     * Performs a lookup of container-related actions at the specified block.
+     *
+     * @param block  The block to look up
+     * @param offset Time constraint in seconds (0 means no time constraint)
+     * @return List of results in a String array format
+     */
+    public static List<ContainerResult> performContainerLookup(Block block, int offset) {
+        List<ContainerResult> result = new ArrayList<>();
+
+        if (!Config.getGlobal().API_ENABLED) {
+            return result;
+        }
+
+        if (block == null) {
+            return result;
+        }
+
+        try (Connection connection = Database.getConnection(false, 1000)) {
+            if (connection == null) {
+                return result;
+            }
+
+            int x = block.getX();
+            int y = block.getY();
+            int z = block.getZ();
+            int time = (int) (System.currentTimeMillis() / 1000L);
+            int checkTime = 0;
+
+            if (offset > 0) {
+                checkTime = time - offset;
+            }
+
+            try (PreparedStatement statement = createLocationQuery(connection, "container", "time,user,type,data,amount,metadata,action,rolled_back", block, checkTime)) {
+                try (ResultSet results = statement.executeQuery()) {
+                    while (results.next()) {
+                        int resultUserId = results.getInt("user");
+                        int resultAction = results.getInt("action");
+                        int resultType = results.getInt("type");
+                        int resultData = results.getInt("data");
+                        long resultTime = results.getLong("time");
+                        int resultAmount = results.getInt("amount");
+                        int resultRolledBack = results.getInt("rolled_back");
+                        byte[] resultMetadata = results.getBytes("metadata");
+
+                        String resultUser = ConfigHandler.playerIdCacheReversed.get(resultUserId);
+
+                        ContainerResult containerResult = new ContainerResult(
+                                resultTime, resultUser, block.getWorld().getName(), x, y, z,
+                                resultType, resultData, resultAmount, resultMetadata,
+                                resultAction, resultRolledBack
+                        );
+
+                        result.add(containerResult);
+                    }
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
